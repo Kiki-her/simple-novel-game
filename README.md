cb4b5d5c45981a60868fba1fa9ad43edd8970350
kiki <k.kazaneko@gmail.com>
2022-12-12 14:05:11 +0900

    create scenes

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # simple-novel-game
-my first game program. This is very simple novel game.
+
+my first game product. This is very simple novel game.
+this product is almost all of using the Qiita article's code.
+My practice for Phaser.js and typescript.
+
+## How to use
+
+npx webpack-dev-server でサーバーを立ち上げる
+
+##
---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import * as Phaser from 'phaser';
+import { Scenes } from './scene';  // 追加
+
+// MySceneはもう使わないので削除
+
+const config: Phaser.Types.Core.GameConfig = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  zoom: window.devicePixelRatio,
+  parent: 'game-app',
+  scene: Scenes  // 変更
+};
+
+new Phaser.Game(config);
